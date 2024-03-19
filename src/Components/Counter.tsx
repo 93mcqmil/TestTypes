4ae42888ec44c7ad68434f605903180996495e06
93mcqmil <mcqueen_1993@hotmail.com>
2024-03-19 16:26:55 +0100

    demo#4 event handling

--- src/Components/Counter.tsx
+++ src/Components/Counter.tsx
@@ -3,11 +3,18 @@ import { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
 
+  const handleAddClick = () => {
+    setCount(count + 1);
+  };
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setCount(Number(e.target.value));
+  };
+
   return (
     <>
       <div>
-        <h1>{count}</h1>
-        <button onClick={() => setCount(count + 1)}>Add</button>
+        <input type='number' value={count} onChange={handleChange} />
+        <button onClick={handleAddClick}>Add</button>
         <button onClick={() => setCount(count - 1)}>Remove</button>
       </div>
     </>
